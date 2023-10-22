400472190e99be82b16b2a05ce1b2da2e3dc2fe5
vicen <Vicenteblasini@gmail.com>
2023-10-22 22:20:29 +0200

    update

--- Tables and variables.sql
+++ Tables and variables.sql
@@ -1,3 +1,7 @@
+--El punto de este script es que tenga todo lo necesario para formatear las tablas y dejarlas como nosotros las usamos
+--Deberia estar hecho de una manera que corriendo todas las lineas quie seran 100 algo, se formateen las tablas sin ningun error
+--que me recuerde son 3 cosas:
+
 /*
 SELECT *
 INTO batting2
